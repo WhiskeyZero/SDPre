aeb7ea1849527b68c8e54c641383ef65807771de
WhiskeyZero <jameswilliampool@gmail.com>
2023-09-28 07:50:57 -0400

    complete objective7lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,11 @@
+public class Objective7Lab4 {
+	public static void main(String[] args) {
+		int n = 20, sum = 0, i = 1;
+
+		while(i <= 20) {
+			sum = sum + i;
+			i++;
+		}
+		System.out.println(sum);
+	}
+}
