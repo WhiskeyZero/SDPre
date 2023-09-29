0c609b73b485316908d7f706bc99ddb90991e314
WhiskeyZero <jameswilliampool@gmail.com>
2023-09-29 05:53:25 -0400

    INCOMPLETE; unable to fix menu; same issue at Obj7Lab5; uplaoding to review and correct later

---
+++ Labs/Objective9Lab3.java
@@ -0,0 +1,22 @@
+public class Objective9Lab3 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int selection;
+
+    while(true){
+
+      
+    }
+
+    scanner.close();
+
+  }
+
+  public static void printMenu() {
+    System.out.println("_____Menu_____");
+    System.out.println("1: Say Hello");
+    System.out.println("2: List My favorite foods");
+    System.out.println("3: Exit");
+    System.out.println();
+  }
+}
